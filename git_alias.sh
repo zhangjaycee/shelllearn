31af5355f49b47692bf4ae66e1c47e4321a7dc24
Jaycee <yxzjc@126.com>
2015-01-23 08:41:01 +0800

    complete git_alias.sh

--- git_alias.sh
+++ git_alias.sh
@@ -2,4 +2,12 @@
 #filename:git_alias.sh
 #20150122 12:34 byJaycee
 
+alias gst="git status"
+alias glog="git log"
+alias grlg="git reflog"
+alias gbrc="git branch"
+alias gcmt="git commit -m"
+alias gpush="git push origin master"
+alias gadd="git add"
+
 
