76ca3aa14b3d59cd53d2c96c7b6c2e25c5651f17
Jaycee <yxzjc@126.com>
2015-01-25 00:38:05 +0800

    add interactive.sh

---
+++ interactive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#filename:interactive.sh
+#20150124 23:46 byJaycee
+#zhjcyx@gmail.com
+
+read -p "please enter your number:" no
+read -p "please enter your name:  " name
+echo You have entered $no,$name
